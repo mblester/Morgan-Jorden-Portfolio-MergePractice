2432a1d5d7e6b8b7d0dc9393b58fa2e0a61d8482
Ivan Vrouwe <ivan.donald.vrouwe@gmail.com>
2016-03-22 23:33:15 -0600

    Added more <nav> functionality; organized CSS in a SMACSS -style structure.

---
+++ assets/js/min/main-min.js
@@ -0,0 +1,1 @@
+!function(){var e=document.querySelector(".copyright-info"),c=document.createTextNode(e.childNodes[0].nodeValue.replace("© ","© "+(new Date).getFullYear()+" "));e.replaceChild(c,e.childNodes[0])}();

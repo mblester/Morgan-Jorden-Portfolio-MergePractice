88892deb919dc06398958587f280a89aaa7aad7a
mblester <morganmurry@gmail.com>
2022-03-22 19:57:12 -0500

    landing page push

--- assets/js/min/main-min.js
+++
@@ -1,1 +0,0 @@
-!function(){var e=document.querySelector(".copyright-info"),c=document.createTextNode(e.childNodes[0].nodeValue.replace("© ","© "+(new Date).getFullYear()+" "));e.replaceChild(c,e.childNodes[0])}();
